1708dc8b2e752d683cd3ecbb9f7cfaad0cd4ca49
Tess Starr <tess.m.starr@gmail.com>
2026-04-01 10:38:44 -0400

    Pushing to make note of what settings work for river and clancy

--- main.py
+++ main.py
@@ -2,24 +2,26 @@ from fileLoader import load_audio_files
 from pitchHandler import pitchshift
 
 
-def manipulatevoices(Files, newfolder): 
-    pitchshift(Files['Bella'], 0.9, newfolder, storagefolder)  
-    #pitchshift(Files['River'], 1.12, newfolder, storagefolder)  
-    #pitchshift(Files['Clancy'], 1.09, newfolder, storagefolder)  
-    #pitchshift(Files['Matt'], 0.9, newfolder, storagefolder)  
-
-
 if __name__ == "__main__":
     #names of the voices in elevenlabs 
     names = ["Bella", "Clancy", "Matt", "River"] #Name of the three voices we are using 
 
 
     #define the folder that the stimuli that has the stim you want to change 
-    #// TODO Make it so it stops saving the new files in the folder with the old files 
+
     storagefolder= 'C:/Users/testarr/Documents/pythoncode/PitchAnalysis/FinalWav' 
     audio_files, GibFiles, IEEEFiles = load_audio_files(storagefolder, names) # function creats a list of wav files in the folder 
 
-    newfolder = "New"
-    manipulatevoices(IEEEFiles, newfolder)
-    manipulatevoices(GibFiles, newfolder)
+    newfolder = "MatchPitches"
+
+    pitchshift(GibFiles['Bella'], 0.8767, newfolder, storagefolder)  
+    #pitchshift(GibFiles['River'], 1.129, newfolder, storagefolder)  ##1.129 gets to 192
+    #pitchshift(GibFiles['Clancy'], 1.071, newfolder, storagefolder)  ##1.071 gets to 90
+    pitchshift(GibFiles['Matt'], 0.9329, newfolder, storagefolder) 
+
+    pitchshift(IEEEFiles['Bella'], 0.9142, newfolder, storagefolder)  
+    #pitchshift(IEEEFiles['River'], 1.1228, newfolder, storagefolder)  ## 1.1228 gets to 192
+    #pitchshift(IEEEFiles['Clancy'], 1.046, newfolder, storagefolder)  ##1.046 gets to 90 
+    pitchshift(IEEEFiles['Matt'], 0.9427, newfolder, storagefolder) 
+
     print ("Files made")
